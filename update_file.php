b7b2ba45de9799f8cde75bd30f86c96a70d99e0e
skyflo1993 <107820935+skyflo1993@users.noreply.github.com>
2022-06-20 09:24:20 +0700

    Update update_file.php

--- update_file.php
+++ update_file.php
@@ -1,12 +1,44 @@
 {
-   "files": [
-     {
-         "name": "SG_test",
-         "url": "https://raw.githubusercontent.com/skyflo1993/new_world_vpn/main/sg/ais/SG_01_AIS.ovpn"
-      }
-],
-         "title": "ทดสอบแอพ" ,
-         "version": 1,
-         "message":"test App"
-         
-}
+    "files": [
+        {
+            "name": "TH_1_AIS",
+            "url": "https://raw.githubusercontent.com/skyflo1993/new_world_vpn/main/inet/AIS.ovpn"
+        },
+        {
+            "name": "TH_1_DTAC_HAPPY",
+            "url": "https://raw.githubusercontent.com/skyflo1993/new_world_vpn/main/inet/DTAC_HAPPY.ovpn"
+        },
+        {
+            "name": "TH_1_DTAC_LINE",
+            "url": "https://raw.githubusercontent.com/skyflo1993/new_world_vpn/main/inet/DTAC_LINE.ovpn"
+        },
+        {
+            "name": "TH_1_DTAC_NOPRO",
+            "url": "https://raw.githubusercontent.com/skyflo1993/new_world_vpn/main/inet/DTAC_NOPRO.ovpn"
+        },
+        {
+            "name": "TH_1_DTAC_SHOPEE",
+            "url": "https://raw.githubusercontent.com/skyflo1993/new_world_vpn/main/inet/DTAC_SHOPEE.ovpn"
+        },
+        {
+            "name": "TH_1_DTAC_SOCIAL",
+            "url": "https://raw.githubusercontent.com/skyflo1993/new_world_vpn/main/inet/DTAC_SOCIAL.ovpn"
+        },
+        {
+            "name": "TH_1_TRUE_FOR_HOME",
+            "url": "https://raw.githubusercontent.com/skyflo1993/new_world_vpn/main/inet/TRUE_FOR_HOME.ovpn"
+        },
+        {
+            "name": "TH_1_TRUE_NOPRO",
+            "url": "https://raw.githubusercontent.com/skyflo1993/new_world_vpn/main/inet/TRUE_NOPRO.ovpn"
+        },
+        {
+            "name": "TH_1_TRUE_SOCIAL",
+            "url": "https://raw.githubusercontent.com/skyflo1993/new_world_vpn/main/inet/TRUE_SOCIAL.ovpn"
+        }
+ ],
+          "title": "" ,
+          "version": 1,
+          "message":"อัพดตไฟล์ใหม่บนแอพเวอร์ชั่นใหม่ \nโปรดอัพเดตแอปให้เป็นเวอร์ชั่นล่าสุด"
+          
+ }
