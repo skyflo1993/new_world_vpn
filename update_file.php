53c5cbba1aa5bedfc07bfc2e91391df69be3c8d0
skyflo1993 <107820935+skyflo1993@users.noreply.github.com>
2022-06-20 09:08:37 +0700

    Update update_file.php

--- update_file.php
+++ update_file.php
@@ -1,16 +1,8 @@
 {
    "files": [
      {
-         "name": "SG_06_DTAC_NOPRO",
-         "url": "https://folksiest-districts.000webhostapp.com/new_world_vpn/sg/dtac_nopro/SG_06_DTAC_NOPRO.ovpn"
-      },
-     {
-         "name": "INET_DTAC_TEST",
-         "url": "https://folksiest-districts.000webhostapp.com/new_world_vpn/inet/DTAC_NOPRO.ovpn"
-      },
-     {
-         "name": "SG_DTAC_NOPRO",
-         "url": "https://folksiest-districts.000webhostapp.com/new_world_vpn/inet/DTAC_NOPRO.ovpn"
+         "name": "SG_test",
+         "url": "https://raw.githubusercontent.com/skyflo1993/new_world_vpn/main/sg/ais/SG_01_AIS.ovpn"
       }
 ],
          "title": "ทดสอบแอพ" ,
